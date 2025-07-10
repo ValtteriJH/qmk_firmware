63f41bf90c9c9d88dd26e9df7b49925142b7956c
vjh <valtteri.huhdankoski@hotmail.com>
2025-07-10 23:43:56 +0300

    Add more modifiers on bottom row

--- keyboards/sofle/keymaps/swider/keymap.c
+++ keyboards/sofle/keymaps/swider/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MINS, KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BACKSLASH,
   KC_CAPS_LOCK, KC_A, KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT, KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_MUTE,     XXXXXXX, KC_B,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                 KC_LCTL, KC_LALT, MO_SYM,MT(MOD_LALT, KC_BSPC) , KC_LCTL,      KC_ENT, KC_SPC, MO_NAV, KC_RALT, KC_RCTL
+  KC_LSFT, KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,   KC_MUTE,     LCTL(KC_0), KC_B,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+  KC_LCTL, KC_LALT, MO_SYM,MT(MOD_LALT, KC_BSPC) , KC_LCTL,      KC_ENT, KC_SPC, MO_NAV, KC_LSFT, KC_RCTL
 ),
 
 // XXXXXXX is disable key, _______ is keep base key
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,                          KC_PGUP, C(KC_LEFT),   KC_UP, C(KC_RGHT), C(KC_BSPC), KC_BSPC,
   _______, KC_LALT,  KC_MPRV,  KC_MPLY, KC_MNXT, KC_INS,                        KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
   _______, _______,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX, _______,       _______,  XXXXXXX, KC_HOME, XXXXXXX, KC_END,   XXXXXXX, _______,
-                         _______, _______, _______, KC_DEL,_______ ,       _______, _______, _______, _______, _______
+                         _______, _______, _______, KC_DEL,_______ ,       _______, _______, _______, KC_X, _______
 ),
 
 // _ADJUST from default
