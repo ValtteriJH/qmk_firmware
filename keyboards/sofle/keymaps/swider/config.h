c910cace133f6b6d1b18df7d93e8be2f54f1c6ed
vjh <valtteri.huhdankoski@hotmail.com>
2025-07-28 15:21:17 +0300

    Add computer agnostic i3 modtap, make modtap keys responsive instantly

--- keyboards/sofle/keymaps/swider/config.h
+++ keyboards/sofle/keymaps/swider/config.h
@@ -30,4 +30,6 @@
 #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
 #define RGB_MATRIX_DEFAfULT_SPD 127 // Sets the default animation speed, if none has been set
 
+#define HOLD_ON_OTHER_KEY_PRESS
+
 #endif
